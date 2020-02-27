690632ce06c19fdbc07cb8be1d9fd35775d8edf8
yabukichi <37928681+yabukichi@users.noreply.github.com>
2020-02-27 16:15:04 +0900

    水玉浮遊を追加

--- js/particles.js
+++ js/particles.js
@@ -1,15 +1,10 @@
 bubbly({
-    // colorStart: '#fff4e6',
-    // colorStop: '#ffe9e4',
-    // blur: 4,
-    // compose: 'source-over',
-    // bubbleFunc:() => `hsla(${Math.random() * 50}, 100%, 50%, .3)`
     colorStart: "#fff",
     colorStop: "#fff",
     blur: 1,
-    bubbles: 20,
+    bubbles: 13,
     compose: "source-over",
     shadowColor: "#fff",
-    radiusFunc: () => Math.random() * 15,
+    radiusFunc: () => Math.random() * 8,
     bubbleFunc: () => `hsla(${Math.random() * 360}, 100%, 50%, .5)`, // 色相,彩度,明度,透明度
   });
