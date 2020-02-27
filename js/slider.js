d2a4b0de5bd4f850cefb8229971e07cfb2329d55
yabukichi <37928681+yabukichi@users.noreply.github.com>
2020-02-27 11:55:25 +0900

    徳田さんからいただいたイベントページをマージ。その他タイトル位置や空間を調整。

---
+++ js/slider.js
@@ -0,0 +1,26 @@
+$(function () {
+    $('.slick').on('init', function (event, slick) {
+        $(this).append('<div class="slick-counter"><span class="current"></span> / <span class="total"></span></div>');
+        $('.current').text(slick.currentSlide + 1);
+        $('.total').text(slick.slideCount);
+      })
+      .slick({
+        dots: true,
+        centerPadding: '0',
+        slidesToShow: 3,
+        centerMode: false,
+        responsive: [{
+          breakpoint: 769,
+          settings: {
+            arrows: false,
+            centerMode: false,
+            slidesToShow:2,
+          }
+        }]
+      })
+  
+    .on('beforeChange', function (event, slick, currentSlide, nextSlide) {
+      $('.current').text(nextSlide + 1);
+    });
+  
+  });
